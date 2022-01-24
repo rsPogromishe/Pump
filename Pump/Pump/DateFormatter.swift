d57ad9953c3cad0329f79dd89389b167e7704bfa
Vladislav Ivshin <wenomok@gmail.com>
2022-01-24 18:28:34 +0400

    profile screen review

--- Pump/Pump/DateFormatter.swift
+++ Pump/Pump/DateFormatter.swift
@@ -8,10 +8,13 @@
 import Foundation
 
 class DateFormat {
+    #warning("Метод в себя получается сегодняшнюю дату")
     static func dateToday(day: Date) -> String {
+        #warning("Потом она здесь снова инициализируется и используется уже она, а не переданная в метод, тогда для чего метод в себя берёт дату?")
         let date = Date()
         let format = DateFormatter()
         format.dateFormat = "EEEE, MMMM dd"
+        #warning("А потом здесь создаётся новая переменная String с таким же именем, что и переданная в метод")
         let day = format.string(from: date)
         return day
     }
